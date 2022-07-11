2ae108552d00d4cec931284a38ade4155482f074
kanekotaiyo <taiyo.yokohama.0925@outlook.jp>
2022-07-11 18:06:26 +0900

    ブログ投稿の作成08-4

--- app/Post.php
+++ app/Post.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    protected $fillable = [
+        'title',
+        'body',
+    ];
     public function getPaginateByLimit(int $limit_count = 10)
     {
         // updated_atで降順に並べたあと、limitで件数制限をかける
